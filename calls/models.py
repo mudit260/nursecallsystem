a8651b7eede6795ef4e6ccf1d6fd2c4d52bfbde8
mudit260 <145419403+mudit260@users.noreply.github.com>
2025-10-11 17:09:39 +0530

    Update models.py

--- calls/models.py
+++ calls/models.py
@@ -7,7 +7,7 @@ class Hospital(models.Model):
     name = models.CharField(max_length=100, unique=True)
     #city = models.CharField(max_length=100, blank=True, null=True)  # optional
     created_at = models.DateTimeField(auto_now_add=True)
-
+    address = models.TextField()
     def __str__(self):
         return f"{self.name} ({self.city})" if self.city else self.name
 
