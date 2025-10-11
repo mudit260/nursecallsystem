cf5fa31e55a237636708f8e97323436eb6c4f40a
mudit260 <145419403+mudit260@users.noreply.github.com>
2025-10-11 14:13:20 +0530

    Update models.py

--- calls/models.py
+++ calls/models.py
@@ -5,7 +5,7 @@ import random
 
 class Hospital(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    city = models.CharField(max_length=100, blank=True, null=True)  # optional
+    #city = models.CharField(max_length=100, blank=True, null=True)  # optional
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
