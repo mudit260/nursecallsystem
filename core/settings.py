f703247e632bb18c8c4c8f9432d6ff315366dea1
mudit260 <145419403+mudit260@users.noreply.github.com>
2025-10-11 15:16:13 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "test-secret-key")  # change for pro
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = [
-    "https://nursecallsystem-71st.onrender.com",
+    "nursecallsystem-71st.onrender.com",
     "localhost",
     "127.0.0.1",
 ]
