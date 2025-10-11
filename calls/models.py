c7239941216425ab8f17883d559cf5482519446a
mudit260 <145419403+mudit260@users.noreply.github.com>
2025-10-11 17:32:57 +0530

    Update models.py

--- calls/models.py
+++ calls/models.py
@@ -6,8 +6,9 @@ import random
 class Hospital(models.Model):
     name = models.CharField(max_length=100, unique=True)
     #city = models.CharField(max_length=100, blank=True, null=True)  # optional
-    created_at = models.DateTimeField(auto_now_add=True)
     address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return f"{self.name} ({self.city})" if self.city else self.name
 
